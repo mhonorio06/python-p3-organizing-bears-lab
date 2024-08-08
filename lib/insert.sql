0d52fec1a4ffb96ea8b715498850b6e81d244af4
Manny <mhfernan35277@gmail.com>
2024-08-08 06:27:10 -0700

    add file

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears Values(1,"Mr. Chocolate", 10, "M", "brown", "calm",1);
+INSERT INTO bears Values(2,"Rowdy", 7, "M", "blackn", "intense",0);
+INSERT INTO bears Values(3,"Tabitha", 7, "F", "brown", "playful",1);
+INSERT INTO bears Values(4,"Seargant Brown", 7, "M", "white", "rash",0 );
+INSERT INTO bears Values(5,"Melissa", 10, "F", "black", "angry",1 );
+INSERT INTO bears Values(6,"Grinch", 18, "M", "brown", "goofy",1 );
+INSERT INTO bears Values(7,"Wendy", 2, "F", "beige", "aggressive",0 );
+INSERT INTO bears Values(8,NULL, 3, "M", "brown", "naive",1 )
